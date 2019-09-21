17d561b170c3448b13c522f3638e02a62428c639
Hrvoje Baic <baic989@gmail.com>
2019-09-21 16:47:14 +0200

    Initial Commit

---
+++ ParticleSwarm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ParticleSwarm
+//
+//  Created by Hrvoje Baic on 21/09/2019.
+//  Copyright © 2019 Hrvoje Baic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ParticleSwarm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ParticleSwarm
+//
+//  Created by Hrvoje Baic on 21/09/2019.
+//  Copyright © 2019 Hrvoje Baic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
